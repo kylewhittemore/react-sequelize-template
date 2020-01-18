62dcf4863de286035a3f10ba2c5df04ffbee1c5c
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-01-18 13:09:56 -0500

    added path dependency to router index

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const path = require('path');
+// const apiRoutes = require('./api');
 const authRoutes = require('./auth/auth-routes');
 
 // router.use('/api', apiRoutes);
 
 router.use('/auth', authRoutes);
 router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 module.exports = router;
