2e9d06f78c0313fb21aef9572a38df1fad445dcf
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-01-19 10:21:36 -0500

    uncommented spa routing fix in server.js

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // -app.get('/', function (req, res) {
 app.use('/api', routes);
 
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Sync sequelize models then start Express app
 // =============================================
