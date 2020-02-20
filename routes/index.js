8d927f0e15a648a566860dfd4a7b9d2b086763a6
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-02-19 21:29:44 -0500

    changed static route to *

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authRoutes = require('./auth/auth-routes');
 const apiRoutes = require('./api/index');
 
-router.use('/', apiRoutes)
+router.use('/', apiRoutes);
 
 router.use('/auth', authRoutes);
 
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api', routes);
 
-app.get('/**', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
