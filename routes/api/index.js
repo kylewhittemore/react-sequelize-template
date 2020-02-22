d9fca78075a8e69e4ae462688fdc608bc5fcc36d
Kyle Whittemore <kyle.whittemore@gmail.com>
2020-02-22 08:37:47 -0500

    changed quote in api response

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const isAuthenticated = require('../../config/middleware/isAuthenticated');
 
 router.get('/secrets', isAuthenticated, (req, res) => {
-  res.json('Talk is cheap. Show me the code. -Linus Torvalds 5/2/2000');
+  res.json('Talk is cheap. Show me the code. -Linus Torvalds');
 });
 
 module.exports = router;
